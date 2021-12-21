ba16534c39e93b2a5ed7371b31ea8f6c36c0c4cc
George Tevosov <megagosha@Georges-MacBook-Pro.local>
2021-12-21 17:37:54 +0300

    app.module fix

--- app/src/app.module.ts
+++ app/src/app.module.ts
@@ -10,13 +10,12 @@ import { GameStatistic } from './game/gamestats.entity';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { rootPath, renderPath } from './constants';
-import { TypeOrmModule } from "@nestjs/typeorm";
-import { Chat } from "./chat/model/chat.entity";
-import { UserChatLink } from "./chat/model/user-chat-link.entity";
-import { Message } from "./chat/model/message.entity";
-import "reflect-metadata";
-import "es6-shim";
-import { ChatModule } from "./chat/chat.module";
+import { Chat } from './chat/model/chat.entity';
+import { UserChatLink } from './chat/model/user-chat-link.entity';
+import { Message } from './chat/model/message.entity';
+import 'reflect-metadata';
+import 'es6-shim';
+import { ChatModule } from './chat/chat.module';
 
 @Module({
   controllers: [AppController],
@@ -26,8 +25,7 @@ import { ChatModule } from "./chat/chat.module";
     UserModule,
     ChatModule,
     ServeStaticModule.forRoot({
-      rootPath: rootPath,
-      rootPath: "/var/www/app/",
+      rootPath: '/var/www/app/',
     }),
     TypeOrmModule.forRoot({
       host: process.env.DB_HOST,
