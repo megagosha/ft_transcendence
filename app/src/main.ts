a4e3fd6ba774d2891e7ffc1916e3f6fec1636095
mcarolee <mcarolee@student.21-school.ru>
2022-02-10 23:32:39 +0500

    nestjs-angular-postgres initial connection

--- app/src/main.ts
+++ app/src/main.ts
@@ -39,6 +39,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('/swagger-ui', app, document);
 
-  await app.listen(3000);
+  await app.listen(3000, '0.0.0.0');
 }
 bootstrap();
